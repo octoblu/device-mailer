b016f012ab34824a65097e569fe31160b7029c8b
redaphid <a@octoblu.com>
2016-03-23 17:46:26 -0700

    updated script to show that even more restrictive permissions still have the bug

--- scripts/test-subscriptions.sh
+++ scripts/test-subscriptions.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
-RECEIVED_MESSAGE_URL="http://requestb.in/168alx11"
+RECEIVED_MESSAGE_URL="http://requestb.in/1242cxn1"
 MESSENGER_DEVICE_UUID=$(meshblu-util register -o | tee messenger-device.json | jq -r '.uuid')
 
 RECEIVER_DEVICE_UUID=$(meshblu-util register -d "{\
- \"receiveWhitelist\": [\"$MESSENGER_DEVICE_UUID\"]\
-, \"sendWhitelist\": []\
+ \"receiveWhitelist\": []\
+, \"sendWhitelist\": [\"*\"]\
+, \"discoverWhitelist\": [\"*\"]\
 , \"configureWhitelist\": []\
-, \"discoverWhitelist\": []\
+, \"receiveAsWhitelist\": []\
+, \"sendAsWhitelist\": []\
 }" | tee receiver-device.json | jq -r '.uuid')
 
 SUBSCRIBER_DEVICE_UUID=$(meshblu-util register -d "{\
  \"receiveWhitelist\": []\
-, \"sendWhitelist\": [\"*\"]\
+, \"sendWhitelist\": []\
 , \"configureWhitelist\": []\
 , \"discoverWhitelist\": []\
 }" | tee subscriber-device.json | jq -r '.uuid')
 
-meshblu-util update -d "{\
- \"receiveAsWhitelist\": [\"$SUBSCRIBER_DEVICE_UUID\"]\
-}" ./receiver-device.json
+# meshblu-util update -d "{\
+#  \"receiveAsWhitelist\": [\"$SUBSCRIBER_DEVICE_UUID\"]\
+# }" ./receiver-device.json
 
 meshblu-util subscription-create -e $RECEIVER_DEVICE_UUID -t received ./subscriber-device.json
 meshblu-util create-hook --type received --url $RECEIVED_MESSAGE_URL ./subscriber-device.json
